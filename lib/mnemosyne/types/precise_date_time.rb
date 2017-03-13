53dc7f2bb0cd1e6c3c4c5cf7567234f53622d246
Jan Graichen <jgraichen@altimos.de>
2017-03-13 16:55:47 +0100

    Update precise datetime type to Rails 5

--- lib/mnemosyne/types/precise_date_time.rb
+++ lib/mnemosyne/types/precise_date_time.rb
@@ -1,35 +1,19 @@
+# frozen_string_literal: true
 module Mnemosyne
   module Types
     class PreciseDateTime < ActiveRecord::Type::Value
-      def type_cast_for_database(value)
-        return super unless value.acts_like?(:time)
 
-        value = value.getutc
-
-        ::Mnemosyne::Clock.to_tick value
+      def serialize(value)
+        ::Mnemosyne::Clock.to_tick(value)
       end
 
-      def type_cast_from_database(value)
-        return unless value
-
-        ::Mnemosyne::Clock.to_time value
-      end
-
-      def klass
-        nil
-      end
-
-      def type
-        nil
-      end
-
-      def cast_value(value)
-        if value.is_a?(Fixnum)
+      def cast(value)
+        if value.is_a?(Integer)
           ::Mnemosyne::Clock.to_time value
         elsif value.is_a?(String) && value =~ /^\d+$/
           ::Mnemosyne::Clock.to_time Integer(value)
-        else
-          super
+        elsif value.acts_like?(:time)
+          value.getutc
         end
       end
     end
