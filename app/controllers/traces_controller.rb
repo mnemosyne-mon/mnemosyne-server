6a776edee5c28c428007e0c8c8c33d9f746b72a9
Jan Graichen <jgraichen@altimos.de>
2017-04-19 16:51:57 +0200

    No need to join spans on index nor uniq results

--- app/controllers/traces_controller.rb
+++ app/controllers/traces_controller.rb
@@ -3,13 +3,11 @@
 class TracesController < ApplicationController
   def index
     traces = Trace
-      .joins(:spans)
       .includes(:spans)
       .includes(:application)
       .where(name: 'app.web.request.rack')
       .order(start: :desc)
       .limit(200)
-      .uniq
 
     render locals: {traces: traces}
   end
