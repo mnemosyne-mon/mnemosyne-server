60673483ed5957c8f8b8beea37d17c891e0b1efa
Jan Graichen <jgraichen@altimos.de>
2017-05-15 12:27:12 +0200

    Adjust settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable debug logging
+  config.log_level = :debug
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
