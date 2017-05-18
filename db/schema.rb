812a0ea2d9fe983648881ea012f159fba9afd523
Jan Graichen <jgraichen@altimos.de>
2017-05-18 14:56:19 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,56 +17,56 @@ ActiveRecord::Schema.define(version: 20170504131755) do
   enable_extension "uuid-ossp"
 
   create_table "activities", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.uuid     "platform_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "platform_id", null: false
   end
 
   create_table "applications", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "name"
-    t.uuid     "platform_id", null: false
-    t.index ["platform_id", "name"], name: "index_applications_on_platform_id_and_name", unique: true, using: :btree
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.uuid "platform_id", null: false
+    t.index ["platform_id", "name"], name: "index_applications_on_platform_id_and_name", unique: true
   end
 
   create_table "platforms", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name"
-    t.string   "title"
+    t.string "name"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_platforms_on_name", unique: true, using: :btree
+    t.index ["name"], name: "index_platforms_on_name", unique: true
   end
 
   create_table "spans", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.uuid     "trace_id",   null: false
-    t.string   "name",       null: false
-    t.bigint   "start",      null: false
-    t.bigint   "stop",       null: false
-    t.jsonb    "meta"
+    t.uuid "trace_id", null: false
+    t.string "name", null: false
+    t.bigint "start", null: false
+    t.bigint "stop", null: false
+    t.jsonb "meta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_spans_on_name", using: :btree
-    t.index ["start"], name: "index_spans_on_start", using: :btree
-    t.index ["trace_id"], name: "index_spans_on_trace_id", using: :btree
+    t.index ["name"], name: "index_spans_on_name"
+    t.index ["start"], name: "index_spans_on_start"
+    t.index ["trace_id"], name: "index_spans_on_trace_id"
   end
 
   create_table "traces", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.uuid     "application_id", null: false
-    t.uuid     "activity_id",    null: false
-    t.uuid     "origin_id"
-    t.string   "name",           null: false
-    t.bigint   "start",          null: false
-    t.bigint   "stop",           null: false
-    t.jsonb    "meta"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["name", "start"], name: "index_traces_on_name_start_desc", order: { start: :desc }, using: :btree
-    t.index ["name"], name: "index_traces_on_name", using: :btree
-    t.index ["origin_id", "start"], name: "index_traces_on_origin_start_desc", order: { start: :desc }, where: "(origin_id IS NULL)", using: :btree
-    t.index ["origin_id"], name: "index_traces_on_origin_id", using: :btree
-    t.index ["start"], name: "index_traces_on_start_desc", order: { start: :desc }, using: :btree
+    t.uuid "application_id", null: false
+    t.uuid "activity_id", null: false
+    t.uuid "origin_id"
+    t.string "name", null: false
+    t.bigint "start", null: false
+    t.bigint "stop", null: false
+    t.jsonb "meta"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "start"], name: "index_traces_on_name_start_desc", order: { start: :desc }
+    t.index ["name"], name: "index_traces_on_name"
+    t.index ["origin_id", "start"], name: "index_traces_on_origin_start_desc", order: { start: :desc }, where: "(origin_id IS NULL)"
+    t.index ["origin_id"], name: "index_traces_on_origin_id"
+    t.index ["start"], name: "index_traces_on_start_desc", order: { start: :desc }
   end
 
 end
