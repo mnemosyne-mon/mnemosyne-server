63b6d5125f045b26ed03af17eb058b46070e5e1c
Jan Graichen <jgraichen@altimos.de>
2017-06-03 16:01:28 +0200

    Add new icons
    * Configure favicons sets

---
+++ app/assets/manifest.js
@@ -0,0 +1,8 @@
+import 'favicon/android-chrome-192x192.png'
+import 'favicon/android-chrome-512x512.png'
+import 'favicon/apple-touch-icon.png'
+import 'favicon/favicon.ico'
+import 'favicon/favicon.svg'
+import 'favicon/favicon-16x16.png'
+import 'favicon/favicon-32x32.png'
+import 'favicon/safari-pinned-tab.svg'
