c0c627768a2f826d5319df3114742a41ccd6d4f0
Jan Graichen <jgraichen@altimos.de>
2017-06-03 16:04:34 +0200

    Rework routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   get '/platform/:id', to: 'platforms#show', as: 'platform'
 
   scope path: '/platform/:platform' do
-    get 'traces', to: 'traces#index'
     get 'traces/heatmap', to: 'traces#heatmap', as: 'heatmap'
-    get 'trace/:id', to: 'traces#show', as: 'trace'
-    get 'span/:id', to: 'spans#show', as: 'span'
 
-    get 'errors', to: 'errors#index'
+    resources :traces, only: %i[index show]
+    resources :spans, only: %i[show]
+    resources :applications, only: %i[index]
+    resources :errors, only: %i[index]
   end
 
   root to: 'platforms#index'
