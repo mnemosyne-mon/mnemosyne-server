ab4eacb916d94a53fc7a2bee4814619da6dd77be
Jan Graichen <jgraichen@altimos.de>
2017-12-29 10:03:21 +0100

    Update pipeline base spec
    Add new default middleware and fix order spec

--- spec/lib/server/pipeline_spec.rb
+++ spec/lib/server/pipeline_spec.rb
@@ -8,11 +8,17 @@ RSpec.describe ::Server::Pipeline do
   subject { pipeline.call(1) }
 
   it 'calls pipeline processors in order' do
-    expect(::Server::Pipeline::Rails::ActionController).to \
+    expect(::Server::Pipeline::Metadata::Grape::Endpoint).to \
       receive(:call).with(1).and_yield(2)
 
+    expect(::Server::Pipeline::Metadata::Rails::ActionController).to \
+      receive(:call).with(2).and_yield(3)
+
+    expect(::Server::Pipeline::Metadata::Rails::ActiveJob).to \
+      receive(:call).with(3).and_yield(4)
+
     expect_any_instance_of(::Server::Builder).to \
-      receive(:call).with(2)
+      receive(:call).with(4)
 
     subject
   end
