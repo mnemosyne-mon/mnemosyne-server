be780ec3b061e17bec3ef4d6eb1cdbf3ee957a33
Franz Liedke <franz.liedke@hpi.de>
2019-04-02 17:21:45 +0200

    Stream CSV response instead of buffering it

--- config/application.rb
+++ config/application.rb
@@ -72,14 +72,19 @@ module Server
 
     initializer 'actioncontroller.renderer.csv' do
       ::ActionController::Renderers.add :csv do |list, _opts|
-        send_data(
-          CSV.generate do |csv|
-            list.each do |row|
-              csv << row.as_csv
-            end
-          end,
-          type: Mime[:csv]
-        )
+        encoder = Enumerator.new do |y|
+          list.each do |row|
+            y << row.as_csv.to_csv
+          end
+        end
+
+        headers['Content-Type'] = 'text/csv'
+        headers['Content-Disposition'] = 'attachment; filename="data.csv"'
+        headers['Cache-Control'] ||= 'no-cache'
+        headers['Transfer-Encoding'] = 'chunked'
+        headers.delete('Content-Length')
+
+        Rack::Chunked::Body.new(encoder)
       end
     end
 
