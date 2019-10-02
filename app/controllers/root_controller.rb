3903d392a375dbd596e639831059bf731b0eda70
Franz Liedke <franz@develophp.org>
2019-10-02 12:09:13 +0200

    Explain possible when trace can not be found

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class RootController < ApplicationController
+  layout 'global'
+
   def index
     # TODO
   end
 
   def trace
-    trace = Trace.find params[:id]
+    trace = Trace.find_by id: params[:id]
 
-    redirect_to trace_path(trace.platform, trace)
+    if trace
+      redirect_to trace_path(trace.platform, trace)
+    else
+      render 'traces/trace_not_found', status: 404
+    end
   end
 end
