02f514bee0fbffa1a6b0ef717be518fcbbd72517
Jan Graichen <jgraichen@altimos.de>
2020-03-29 14:05:12 +0200

    Skip transaction on insert
    This might improve the txid wraparound autovacuum pressure.

--- lib/server/builder.rb
+++ lib/server/builder.rb
@@ -14,47 +14,45 @@ module Server
         name: payload.fetch(:application),
         platform: platform.id
 
-      ActiveRecord::Base.transaction do
-        trace ||= begin
-          ::Trace.create! \
-            id: payload[:uuid],
-            origin_id: payload[:origin],
-            application: application,
-            activity_id: payload[:transaction],
-            platform: platform,
-            hostname: payload[:hostname],
-            name: payload[:name],
-            start: ::Server::Clock.to_time(Integer(payload[:start])),
-            stop: ::Server::Clock.to_time(Integer(payload[:stop])),
-            meta: payload[:meta]
-        end
+      trace ||= begin
+        ::Trace.create! \
+          id: payload[:uuid],
+          origin_id: payload[:origin],
+          application: application,
+          activity_id: payload[:transaction],
+          platform: platform,
+          hostname: payload[:hostname],
+          name: payload[:name],
+          start: ::Server::Clock.to_time(Integer(payload[:start])),
+          stop: ::Server::Clock.to_time(Integer(payload[:stop])),
+          meta: payload[:meta]
+      end
 
-        # `Span.column_names` is required to force bulk insert plugin
-        # to process `id` column too.
-        Span.bulk_insert(*Span.column_names) do |worker|
-          Array(payload[:span]).each do |data|
-            worker.add \
-              id: data[:uuid],
-              name: data[:name],
-              trace_id: trace.id,
-              platform_id: trace.platform_id,
-              start: ::Server::Clock.to_time(Integer(data[:start])),
-              stop: ::Server::Clock.to_time(Integer(data[:stop])),
-              meta: data[:meta]
-          end
+      # `Span.column_names` is required to force bulk insert plugin
+      # to process `id` column too.
+      Span.bulk_insert(*Span.column_names) do |worker|
+        Array(payload[:span]).each do |data|
+          worker.add \
+            id: data[:uuid],
+            name: data[:name],
+            trace_id: trace.id,
+            platform_id: trace.platform_id,
+            start: ::Server::Clock.to_time(Integer(data[:start])),
+            stop: ::Server::Clock.to_time(Integer(data[:stop])),
+            meta: data[:meta]
         end
+      end
 
-        payload.fetch(:errors, []).each do |error|
-          Failure.create! \
-            type: error.fetch(:type),
-            text: error.fetch(:text),
-            stop: trace.stop,
-            trace: trace,
-            hostname: trace.hostname,
-            platform: trace.platform,
-            application: trace.application,
-            stacktrace: error.fetch(:stacktrace, [])
-        end
+      payload.fetch(:errors, []).each do |error|
+        Failure.create! \
+          type: error.fetch(:type),
+          text: error.fetch(:text),
+          stop: trace.stop,
+          trace: trace,
+          hostname: trace.hostname,
+          platform: trace.platform,
+          application: trace.application,
+          stacktrace: error.fetch(:stacktrace, [])
       end
     end
   end
