d0fb90e60001bbbd060f01190115c6538a528480
Jan Graichen <jgraichen@altimos.de>
2021-03-31 16:45:53 +0200

    Exp: free form meta search

--- app/controllers/traces_controller.rb
+++ app/controllers/traces_controller.rb
@@ -69,6 +69,10 @@ class TracesController < ApplicationController
     scope.where('(stop - start) < interval ?', ::ActiveSupport::Duration.parse_string(value).iso8601)
   end
 
+  has_scope :meta, type: hash do |_, scope, value|
+    scope.where('meta @> ?', value.to_json)
+  end
+
   has_scope :range, default: true, allow_blank: true do |controller, scope, _|
     scope.range controller.range
   end
