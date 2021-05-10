54de2480d20466a1b2727e6577edf5cc3a62c016
Jan Graichen <jgraichen@altimos.de>
2021-05-10 14:42:00 +0200

    Support loading sentry DSN from rails secrets

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-if ENV['SENTRY_DSN'].present?
+SENTRY_DSN = ENV.fetch('SENTRY_DSN', Rails.application.secrets.sentry_dsn)
+
+if SENTRY_DSN.present?
   Sentry.init do |config|
-    config.dsn = ENV['SENTRY_DSN']
+    config.dsn = SENTRY_DSN
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     config.traces_sample_rate = 1.0
